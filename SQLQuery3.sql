c34a8d49ad2573d0a4ff2743cba8f2c31796daa9
Amar Poddar <amar.poddar@capgemini.com>
2020-09-29 15:36:45 +0530

    Stored Procedure and DB_Script is Added

---
+++ SQLQuery3.sql
@@ -0,0 +1,87 @@
+CREATE TABLE Project(
+	Project_ID INT IDENTITY(101, 1) PRIMARY KEY NOT NULL,
+	Project_Name NVARCHAR(100) NOT NULL,
+	Project_StartDate DATE NOT NULL,
+	Project_EndDate DATE NOT NULL,
+	Project_Details NVARCHAR(300) NOT NULL	
+);
+
+insert into Project  Values('xyz', '2020-09-29', '2020-10-23', 'all');
+insert into Project  Values('abc', '2020-09-29', '2021-12-03', 'alllllllll');
+select * from Project;
+
+CREATE TABLE Employee(
+	Employee_ID INT IDENTITY(1001,1) PRIMARY KEY NOT NULL,
+	Employee_Password NVARCHAR(30) NOT NULL,
+	Employee_Name NVARCHAR(50) NOT NULL,
+	Employee_Email NVARCHAR(50) unique NOT NULL,
+	Employee_Phone_Number NVARCHAR(13) unique NOT NULL,
+	Employee_Role NVARCHAR(30) NOT NULL,
+	Manager_ID INT,
+	Project_Id INT FOREIGN KEY references Project(Project_ID) ON DELETE SET NULL
+);
+
+insert into Employee  Values('password', 'Amar', 'amar@gmail.com','8989789568','Analyst',201,101);
+insert into Employee  Values('pass', 'Amar  Kumar', 'apoddar@gmail.com','848849568','Analyst A4',202,102);
+select * from Employee;
+
+drop table Employee_Project
+CREATE TABLE Employee_Project(
+	Employee_ID INT FOREIGN KEY references Employee(Employee_ID) ON DELETE SET NULL,
+	Project_ID INT FOREIGN KEY references Project(Project_ID),
+	Project_Manager_ID INT FOREIGN KEY references Employee(Employee_ID)
+);
+
+INSERT into Employee_Project values(1001, 101, 1002),(1002,102, 1002);
+select * from Employee_Project;
+
+drop table Attendance
+CREATE TABLE Attendance(
+	Attendance_ID INT IDENTITY PRIMARY KEY NOT NULL,
+	Attedance_Type nvarchar(10) NOT NULL,
+	Attedance_Date DATE NOT NULL,
+	In_Time TIME(0) NOT NULL,
+	Out_Time TIME(0) NOT NULL,
+	Status_Of_Attendance NVARCHAR(10) NOT NULL,
+	Status_Update_Date DATE NOT  NULL,
+	Status_Updated_By INT FOREIGN KEY references Employee(EMPLOYEE_ID) ON DELETE SET NULL,
+	Employee_ID INT FOREIGN KEY references Employee(EMPLOYEE_ID) ,
+	Manager_ID INT FOREIGN KEY references Employee(EMPLOYEE_ID) 
+);
+
+insert into Attendance values('P','2020-09-29','08:30','16:00','A', '2020-08-16', 1001, 1002, 1001),('ASDDD','2020-11-29','09:30','18:00','P', '2020-07-16', 1002, 1001, 1002);
+select * from Attendance;
+
+drop table Leave
+CREATE TABLE Leave(
+	Leave_Request_ID int IDENTITY PRIMARY KEY NOT NULL,
+	LeaveType NVARCHAR(30) NOT NULL,
+	NO_Of_Days int NOT NULL,
+	Leave_Balance int NOT NULL,
+	Leave_Date_From DATE NOT NULL,
+	Leave_Date_To DATE NOT NULL,
+	Leave_Status NVARCHAR(30) NOT NULL,
+	Employee_ID INT FOREIGN KEY references Employee(EMPLOYEE_ID) ON DELETE SET NULL,
+	Manager_ID INT FOREIGN KEY references Employee(EMPLOYEE_ID) 
+
+);
+
+
+insert into Leave values('P',5,2,'2020-09-29','2020-09-30','pending',1001,1002),
+('M',3,4,'2020-10-25','2020-10-30','Approved',1002,1001);
+select * from Leave;
+
+
+CREATE TABLE Admin(
+	Admin_ID int IDENTITY(100001,1) PRIMARY KEY NOT NULL,
+	Admin_Name NVARCHAR(30) NOT NULL,
+	Admin_Password NVARCHAR(30) NOT NULL,
+);
+
+insert into Admin values('Amar','password'),('Kumar','pass');
+select * from Admin;
+select * from Leave;
+Select * from Attendance;
+Select * from Project;
+select * from Employee;
+Select * from Employee_Project;
---
+++ Stored_Procedure_ALMS.sql
@@ -0,0 +1,314 @@
+--PROJECT MODULE.......................................................>
+
+CREATE PROCEDURE spViewProjectDetails
+	@pId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	SELECT Project.Project_ID, Project.Project_Name, Project.Project_StartDate, Project.Project_EndDate, 
+	Project.Project_Details, Employee_Project.Employee_ID, Employee_Project.Project_Manager_ID 
+	FROM Project JOIN Employee_Project
+	on Project.Project_ID = Employee_Project.Project_ID
+	Where Project.Project_ID = @pId;
+END
+GO
+exec spViewProjectDetails 102;
+
+CREATE PROCEDURE spAddProject
+	@projectName NVARCHAR(100),
+	@projectStartDate DATE,
+	@projectEndDate DATE,
+	@projectDetails NVARCHAR(300)
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	INSERT INTO Project VALUES(@projectName, @projectStartDate, @projectEndDate, @projectDetails);
+END
+GO
+
+exec spAddProject 'ABCD', '2020-09-18', '2020-10-19', 'ABCDEFGHIJKL';
+select * from Project;
+
+
+CREATE PROCEDURE spUpdateProject
+	@pId int,
+	@projectName NVARCHAR(100),
+	@projectStartDate DATE,
+	@projectEndDate DATE,
+	@projectDetails NVARCHAR(300)
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	UPDATE Project set Project_Name = @projectName, Project_StartDate = @projectStartDate, 
+	Project_EndDate = @projectEndDate, Project_Details = @projectDetails where Project_ID = @pId;
+END
+GO
+
+
+exec spUpdateProject @projectName ='aaa', @projectStartDate = '2020-08-05', @projectEndDate = '2020-09-08',
+@projectDetails = 'abcdabcd', @pId =101;
+
+select * from Project;
+
+
+CREATE PROCEDURE spDeleteProject
+	@pId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	DELETE FROM Project where Project_ID = @pId;
+END
+GO
+
+exec spDeleteProject 103;
+select * from Project;
+
+CREATE PROCEDURE spAssignProjectToEmployee
+	@eId int,
+	@pId int,
+	@mId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	INSERT INTO Employee_Project VALUES(@eId, @pId, @mId);
+END
+GO
+
+exec spAssignProjectToEmployee 1001, 101, 1002;
+select * from Employee_Project;
+
+
+CREATE PROCEDURE spUpdateEmployeeOnProject
+	@eId int,
+	@pId int,
+	@mId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	UPDATE Employee_Project set Employee_ID = @eId, Project_Manager_ID = @mId where Project_ID = @pId; 
+END
+GO
+
+exec spUpdateEmployeeOnProject @pId = 102, @eId = 1001, @mId = 1002
+
+select * from Employee_Project ;
+
+
+CREATE PROCEDURE spListProject
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	SELECT Project_ID, Project_Name, Project_StartDate, Project_EndDate from Project
+END
+GO
+exec spListProject;
+
+
+--Attendance Module.....................................................................................
+
+Create procedure spAddAttendance
+	@Attedance_Type nvarchar(10),
+	@Attedance_Date DATE,
+	@In_Time TIME(0),
+	@Out_Time TIME(0),
+	@Status_Of_Attendance NVARCHAR(10),
+	@Status_Update_Date DATE,
+	@Status_Updated_By INT ,
+	@Employee_ID INT,
+	@Manager_ID int 
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	insert into Attendance values(@Attedance_Type,@Attedance_Date,@In_Time,@Out_Time,
+	@Status_Of_Attendance,@Status_Update_Date, @Status_Updated_By,@Employee_ID,@Manager_ID);
+END
+GO
+ 
+ exec spAddAttendance 'aa','2020-08-08','20:00:00','20:00:00','gaa','2020-08-6',1001,1002,1001;
+ select * from Attendance;
+
+ /*-------------------------modify attendance------------------------*/
+
+ Create procedure spModifyAttendance
+	@aId int,
+	@Attedance_Type nvarchar(10),
+	@Attedance_Date DATE,
+	@In_Time TIME(0),
+	@Out_Time TIME(0),
+	@Status_Of_Attendance NVARCHAR(10),
+	@Status_Update_Date DATE,
+	@Status_Updated_By INT,
+	@Employee_ID INT,
+	@Manager_ID int 
+as 
+BeGIN
+ SET NOCOUNT OFF;
+	update Attendance set Attedance_Type = @Attedance_Type,
+	Attedance_Date= @Attedance_Date,
+	In_Time= @In_Time,
+	Out_Time= @Out_Time,
+	Status_Of_Attendance = @Status_Of_Attendance,
+	Status_Update_Date= @Status_Update_Date,
+	Status_Updated_By = @Status_Updated_By,
+	Employee_ID = @Employee_ID,
+	Manager_ID = @Manager_ID 
+	where Attendance_ID = @aId;
+ END
+ 
+ exec spModifyAttendance @Attedance_Type ='aaa',@Attedance_Date='2020-08-08',@In_Time='20:00:00',
+ @Out_Time='20:00:00',@Status_Of_Attendance='ga',@Status_Update_Date ='2020-08-06',
+ @Status_Updated_By=1001, @Employee_ID=1002,
+ @Manager_ID=1001 , @aId = 5;
+
+ select * from Attendance;
+
+ /*---------------------delete Attendance---------------------------*/
+
+ CREATE PROCEDURE spDeleteAttendance
+	@aId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	DELETE FROM Attendance where Attendance_ID = @aId;
+END
+GO
+
+exec spDeleteAttendance 6;
+select * from attendance;
+
+/*---------------------------------attandanceDetailsDisplay------------------------*/
+
+CREATE PROCEDURE spAttendanceDetailsDisplay
+
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	select * from Attendance;
+END
+GO
+
+exec spAttendanceDetailsDisplay ;
+
+
+/*---------------------------------ApproveRejectAttendance--------------------------*/
+
+CREATE PROCEDURE spApproveRejectAttendanceRequest
+	@aId int,
+	@Status_Of_Attendance varchar(10)
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	update Attendance set Status_Of_Attendance = @Status_Of_Attendance where Attendance_ID = @aId; 
+END
+GO
+
+exec spApproveRejectAttendanceRequest @status_Of_Attendance = 'a', @aId = 3;
+select * from Attendance;
+
+
+
+
+/* Employee Module---------------------add employee-----------------------*/
+
+CREATE PROCEDURE spAddEmployee	
+	@Employee_Password NVARCHAR(30) ,
+	@Employee_Name NVARCHAR(50),
+	@Employee_Email NVARCHAR(50) ,
+	@Employee_Phone_Number NVARCHAR(13),
+	@Employee_Role NVARCHAR(30),
+	@mId INT,
+	@pId INT 
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	INSERT INTO Employee VALUES(@Employee_Password,@Employee_Name,@Employee_Email,@Employee_Phone_Number
+	,@Employee_Role,@mId, @pId);
+END
+GO
+
+exec spAddEmployee 'password', 'dhara', 'dhara@gmail.com','8888888888','Analyst',202,101;
+select * from employee;
+
+/*-----------------Delete Employee -------------------------*/
+-- here i face some issue.................
+
+--The DELETE statement conflicted with the REFERENCE constraint "FK__Employee___Emplo__29572725". The conflict occurred in database "Project_DB", table "dbo.Employee_Project", column 'Employee_ID'.
+
+CREATE PROCEDURE spDeleteEmployee
+	@eId int
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	DELETE FROM Employee where Employee_ID = @eId;
+END
+GO
+
+exec spDeleteEmployee 1001; 
+select * from employee;
+
+
+
+/*---------------------------Modify employee details -----------------------------*/
+
+CREATE PROCEDURE spModifyEmployeeDetails
+	@eId int,
+	@Employee_Password NVARCHAR(30) ,
+	@Employee_Name NVARCHAR(50),
+	@Employee_Email NVARCHAR(50) ,
+	@Employee_Phone_Number NVARCHAR(13),
+	@Employee_Role NVARCHAR(30),
+	@Manager_ID INT,
+	@Project_Id INT 
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	update Employee set  Employee_Password=@Employee_Password,
+	Employee_Name=@Employee_Name,
+	Employee_Email=@Employee_Email,
+	Employee_Phone_Number=@Employee_Phone_Number,
+	Employee_Role=@Employee_Role,
+	Manager_ID=@Manager_ID,
+	Project_Id=@Project_Id
+	where Employee_ID = @eId;
+END
+GO
+
+exec spModifyEmployeeDetails 
+@eId = 1002,
+	@Employee_Password = 'asdasd' ,
+	@Employee_Name ='dhara',
+	@Employee_Email = 'radha@gmail.com' ,
+	@Employee_Phone_Number= '4545676765',
+	@Employee_Role ='manager',
+	@Manager_ID =1001,
+	@Project_Id =101; 
+
+
+	select * from employee;
+
+	/*--------------------------login logout ---------------------------*/
+
+
+
+CREATE PROCEDURE spLoginEmployee
+	@eId int,
+	@password nvarchar(10)
+AS
+BEGIN
+	SET NOCOUNT OFF;
+	select * from employee where Employee_ID = @eId and Employee_Password =@password;
+END
+GO
+
+exec spLoginEmployee 1002, asdasd;
+
+
+
+
+
+
+
+
+
+
+
